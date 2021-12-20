967eb3466a27e0bf06c4fe4abd30851d7ee82249
Evgeny Mahnovets <evgeny.mahnovets@gmail.com>
2021-12-20 17:43:12 +0200

    Use aliases instead of variables

--- cypress/integration/smoke/menu-items.spec.js
+++ cypress/integration/smoke/menu-items.spec.js
@@ -4,9 +4,6 @@
 import * as faker from 'faker';
 
 describe('Menu Items', () => {
-  let activeMenuItem;
-  let inactiveMenuItem;
-
   beforeEach(() => {
     cy.login();
     cy.createMenuItem({
@@ -15,29 +12,29 @@ describe('Menu Items', () => {
       isActive: true,
       price: faker.datatype.number({ min: 0, max: 10000 }),
       currency: faker.random.arrayElement(['USD', 'EUR']),
-    }).then((response) => {
-      activeMenuItem = response.body;
-    });
+    })
+      .then((response) => response.body)
+      .as('activeMenuItem');
     cy.createMenuItem({
       name: faker.random.word(),
       description: faker.random.words(10),
       isActive: false,
       price: faker.datatype.number({ min: 0, max: 10000 }),
       currency: faker.random.arrayElement(['USD', 'EUR']),
-    }).then((response) => {
-      inactiveMenuItem = response.body;
-    });
+    })
+      .then((response) => response.body)
+      .as('inactiveMenuItem');
     cy.clearLocalStorage();
   });
 
-  it('should display only active items for unauthorized users', () => {
+  it('should display only active items for unauthorized users', function test() {
     cy.visit('/');
     cy.url().should('include', '/menu-items');
 
-    cy.expectItemCard(activeMenuItem.id, {
-      ...activeMenuItem,
-      price: activeMenuItem.price / 100,
+    cy.expectItemCard(this.activeMenuItem.id, {
+      ...this.activeMenuItem,
+      price: this.activeMenuItem.price / 100,
     });
-    cy.dataCy(`menu-item-card-${inactiveMenuItem.id}`).should('not.exist');
+    cy.dataCy(`menu-item-card-${this.inactiveMenuItem.id}`).should('not.exist');
   });
 });
